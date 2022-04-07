fa02ee64c2fc36ed30928192f4a6358ca0e505e4
domino659 <martin@sionfamily.com>
2022-04-07 13:58:18 +0200

    css

--- app/index.php
+++ app/index.php
@@ -3,9 +3,12 @@
 header('Access-Control-Allow-Origin: http://localhost:3000');
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Headers: authorization');
-header('Access-Control-Allow-Methods: GET, POST, OPTION');
 
 // var_dump(getallheaders());
+
+// echo json_encode([
+//     'message' => $_POST
+// ]);
 
 echo json_encode([
     'username' => $_SERVER['PHP_AUTH_USER'],
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,16 @@
 import { useEffect } from "react"
+import './App.css'; 
 
 export default function App() {
   useEffect( () => {
     const body = new URLSearchParams({
-      username: 'Francis',
+      username: 'Domino',
       password: 'password'
     });
 
     const headers = new Headers({
       'Content-Type': 'application/x-www-form-urlencoded',
-      'authorization': `Basic ${btoa("Francis:password")}`
+      'authorization': `Basic ${btoa("banana")}`
     })
 
     fetch( 'http://localhost:2345', {
@@ -24,6 +25,26 @@ export default function App() {
   },  [])
 
   return (
-  <></>
+    <main>
+      <div className="search-box">
+        <form action="">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Username"
+            // value={query}
+            // onKeyPress={search}
+          ></input>
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Password"
+            // value={query}
+            // onKeyPress={search}
+          ></input>
+          <input type="submit" className="submit" />
+        </form>
+        </div>
+    </main>
   )
 }
